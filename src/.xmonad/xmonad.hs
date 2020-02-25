c89ad11bf46939b3bdb31d52fa6af88ef2ed5aa0
Lucas Ramos <lramos@coachit.com.br>
2020-02-25 11:45:04 -0300

    removed xmonad build files, zsh, no polybar for i3 xfce

--- src/.xmonad/xmonad.hs
+++ src/.xmonad/xmonad.hs
@@ -1,7 +1,14 @@
 import XMonad
+import XMonad.Config.Xfce
 
-main = xmonad $ def {
+-- main = xmonad $ def {
+--     modMask = mod4Mask,
+--     borderWidth = 3,
+--     terminal = "$TERMINAL"
+-- }
+
+
+main = xmonad xfceConfig {
     modMask = mod4Mask,
-    borderWidth = 3,
-    terminal = "$TERMINAL"
+    
 }
